ecc1b06d34cc7c1eaa2ceb963d8c600b3c6acc81
fritzgt <tuanis@live.com>
2019-06-26 23:59:07 -0400

    create store and warpped app in provider

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,4 +3,19 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//importing dependencies
+import rootReducer from "./reducers";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "redux-thunk";
+import thunk from "redux-thunk";
+
+//Creating the store
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+//Wrapping the app with the Provider
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
