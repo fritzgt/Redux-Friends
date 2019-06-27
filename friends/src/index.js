8313e3261b91535293eaf0c79b605ff80b6c4fdf
fritzgt <tuanis@live.com>
2019-06-27 00:01:39 -0400

    corrected typo

--- friends/src/index.js
+++ friends/src/index.js
@@ -6,11 +6,12 @@ import App from "./App";
 //importing dependencies
 import rootReducer from "./reducers";
 import { createStore, applyMiddleware } from "redux";
-import { Provider } from "redux-thunk";
+import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { logger } from "redux-logger";
 
 //Creating the store
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 //Wrapping the app with the Provider
 ReactDOM.render(
