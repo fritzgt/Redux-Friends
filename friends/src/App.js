0f5cb93f70b0b7145de8eb88ba358eff1ca0f9db
fritzgt <tuanis@live.com>
2019-06-27 07:54:14 -0400

    added route to login page in that app js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./App.css";
+//Importing react router components
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 //importing login page
-
 import Login from "./components/Login";
 
+//wrapping the app with router and added a route to the login
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Friends</h1>
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Welcome to Friends</h1>
+        <Link to="/login">Login</Link>
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
   );
 }
 
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,15 +1,40 @@
 import React from "react";
 
+//connecting app
+import { connect } from "react-redux";
+
 class Login extends React.Component {
+  state = {
+    userName: "",
+    password: ""
+  };
+
+  changeHandler = e => {};
+
+  submitHandler = e => {
+    e.preventDefault();
+  };
+
   render() {
     return (
-      <form>
-        <input />
-        <input />
+      <form onSubmit={this.submitHandler}>
+        <input
+          type="text"
+          placeholder="User Name"
+          value={this.state.userName}
+          onChange={this.changeHandler}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={this.changeHandler}
+        />
         <button>Login</button>
       </form>
     );
   }
 }
 
-export default Login;
+//needs to be completed
+export default connect(null)(Login);
